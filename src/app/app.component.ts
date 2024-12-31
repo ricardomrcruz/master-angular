5cdff079750705c886aac68566a762559d8c6bf0
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-12-31 02:32:44 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/whishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wishlist';
+  items: WishItem[] | never = [
+    new WishItem('To Learn Angular'),
+    new WishItem('To Get Some coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
+  title = 'heyyyyyyy';
 }
