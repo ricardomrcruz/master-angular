e119d7350d60a2a1f378b64c88cbfc14b3b53d57
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2025-01-02 23:10:27 +0100

    using getter to filter and provide data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/whishItem';
 
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,27 +20,19 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself'),
   ];
 
-  listFilter: String = '0';
+  listFilter: any = '0';
 
   newWishText = '';
 
   title = 'heyyyyyyy';
 
-  visibleItems: WishItem[] = this.items;
+  get visibleItems(): WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  }
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
-  }
-
-  filterChanged(value: any) {
-    if (value === '0') {
-      this.visibleItems = this.items;
-    } else if (value === '1') {
-      this.visibleItems = this.items.filter((item) => !item.isComplete);
-    } else {
-      this.visibleItems = this.items.filter((item) => item.isComplete);
-    }
   }
 
   toggleItem(item: WishItem) {
