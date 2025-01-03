c658f390307923028c96282d1e1fd82e999f5def
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2025-01-04 00:54:43 +0100

    creating a component w input

--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { WishItem } from '../../shared/models/whishItem';
 import { CommonModule } from '@angular/common';
 
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './wish-list.component.css',
 })
 export class WishListComponent {
-  wishes: WishItem[] = [];
+  @Input() wishes: WishItem[] = [];
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
