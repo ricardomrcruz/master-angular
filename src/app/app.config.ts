164b47e4800b08f154b41591a2c3aed682271bc8
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2025-01-11 05:10:20 +0100

    build: update angular bootstrap configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { EventService } from '../shared/services/EventService';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    EventService,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    {
+      provide: 'API_KEY',
+      useValue: process.env['API_KEY']
+    }
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { EventService } from './shared/services/EventService';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes'
+import { appConfig } from './app/app.config';
 
 
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), EventService, provideRouter(routes)],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
