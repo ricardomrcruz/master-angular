cc5c43aa475996a7b1ba6ee29f5c9eecc857c8cb
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2025-01-18 04:13:28 +0100

    style: add angular material theme setup

--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -6,8 +6,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { StepperOrientation } from '@angular/material/stepper';
 import { CommonModule } from '@angular/common';
 import { OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wizard',
@@ -28,6 +30,9 @@ export class WizardComponent implements OnInit {
 
   personalInfoForm: FormGroup;
 
+  isLinear = false;
+
+
   // creates form group for personal info with validation
   constructor(private fb: FormBuilder, private wizardService: WizardService) {
     this.personalInfoForm = this.fb.group({
@@ -42,12 +47,13 @@ export class WizardComponent implements OnInit {
     this.wizardService.getFormState().subscribe(data => {
       this.personalInfoForm.patchValue({
         firstName: data.firstName,
-        lasttName: data.lastName,
+        lastName: data.lastName,
         email: data.email,
       });
     });
   }
 
+  // to save form data when clicking on next step
   savePersonalInfo(): void {
     if (this.personalInfoForm.valid) {
       this.wizardService.updateFormData(this.personalInfoForm.value);
